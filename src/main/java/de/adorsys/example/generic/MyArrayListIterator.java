ad7bc6ac77b1325701eb5354fc13888d6af17dba
Boris Skert <boris.skert@adorsys.de>
2018-08-23 08:36:59 +0200

    clean up code

--- src/main/java/de/adorsys/example/generic/MyArrayListIterator.java
+++ src/main/java/de/adorsys/example/generic/MyArrayListIterator.java
@@ -3,33 +3,32 @@ package de.adorsys.example.generic;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class MyArrayListIterator<E> implements Iterator<E> {
+class MyArrayListIterator<E> implements Iterator<E> {
 
     private final MyArrayList<E> list;
-
     private int position = 0;
 
-    public MyArrayListIterator(MyArrayList<E> list) {
+    MyArrayListIterator(MyArrayList<E> list) {
         this.list = list;
     }
 
     @Override
     public boolean hasNext() {
-
-        if (position < list.size()) {
-            return true;
-        }
-        return false;
+        return hasNextElement();
     }
 
     @Override
     public E next() {
-        if (position < list.size()) {
+        if (hasNextElement()) {
             E element = list.get(position);
             position++;
             return element;
         }
 
         throw new NoSuchElementException();
+    }
+
+    private boolean hasNextElement() {
+        return position < list.size();
     }
 }
